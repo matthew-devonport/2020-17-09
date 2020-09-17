9d3dd977c569772dbdee0ddac626a873a7f9939f
matthew-devonport <roarsintheeast@gmail.com>
2020-09-17 23:55:50 +1200

    completed second questions and added pseudocode

--- src/sales.js
+++ src/sales.js
@@ -6,19 +6,37 @@ const sales = [
     { itemSold: 'Hockey stick', price: 54.95, dateSold: '2017-03-19', id: 'j_999' }
     ]
     
-    /* creating totalPrice function */
+// 1. Return the sum of the price of all properties as a single value.
+
+    // creating totalPrice function 
     function totalPrice() {
 
-    /* mapping over array to get the prices into an array */
+    // mapping over array to get the prices into an array 
     const priceArray = sales.map(item => {
         const prices = item.price  
         
         return prices
     })
-    /* reducing the array to produce the total sum of the prices */
+    // reducing the array to produce the total sum of the prices 
         var sum = priceArray.reduce(function(a, b){
             return a + b;
         }, 0);
     
+    // returning result of the total sum with minimal decimal places    
     return sum.toFixed(2)
     }
+
+// 2. Return the items which were sold in 2017.
+
+   // creating saleDate function 
+    function saleDate() {
+
+        // filtering dateSold properties by 2017 date with startsWidth method
+        const date = "2017"
+        const items = sales.filter(o => o.dateSold.startsWith(date));
+        
+        //returning result which shows the two correct items
+        return items
+        }
+      
+      
