60ad31b2da4748dc705ebf9de23bc03dd1768e67
matthew-devonport <roarsintheeast@gmail.com>
2020-09-17 20:14:54 +1200

    created function and mapped price

---
+++ src/sales.js
@@ -0,0 +1,16 @@
+const sales = [
+    { itemSold: 'Football', price: 19.99, dateSold: '2018-04-07', id: 'j_123' },
+    { itemSold: 'Trainers', price: 159.95, dateSold: '2018-03-02', id: 't_acds1' },
+    { itemSold: 'Cricket bat', price: 204.97, dateSold: '2018-04-05', id: 'j_456'},
+    { itemSold: 'Rugby ball', price: 30.00, dateSold: '2017-04-22', id: 't_acds3' },
+    { itemSold: 'Hockey stick', price: 54.95, dateSold: '2017-03-19', id: 'j_999' }
+    ]
+    
+    function totalPrice() {
+    const priceArray = sales.map(item => {
+        const prices = item.price  
+        
+        return prices
+    })
+
+    }
