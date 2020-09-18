c3ed1672296a529a2aad64c227ea7851b21a55e3
matthew-devonport <roarsintheeast@gmail.com>
2020-09-18 19:55:09 +1200

    added list to sidenav

--- src/App.js
+++ src/App.js
@@ -9,7 +9,15 @@ function App() {
           <div class="header__search">Search...</div>
           <div class="header__avatar">Icon</div>
         </header>
-        <aside className="sidenav"></aside>
+        <aside className="sidenav">
+          <ul class="sidenav__list">
+            <li class="sidenav__list-item">Item One</li>
+            <li class="sidenav__list-item">Item Two</li>
+            <li class="sidenav__list-item">Item Three</li>
+            <li class="sidenav__list-item">Item Four</li>
+            <li class="sidenav__list-item">Item Five</li>
+          </ul>
+        </aside>
         <main className="main"></main>
         <footer className="footer">
           <div class="footer__copyright">&copy; 2020 Matthew Devonport</div>
