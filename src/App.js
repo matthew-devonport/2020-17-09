9be632d0cc47de6aa8d1abb3e5afa1e6dd0c9d06
matthew-devonport <roarsintheeast@gmail.com>
2020-09-19 11:00:40 +1200

    cleaned up classnames/sass headers

--- src/App.js
+++ src/App.js
@@ -16,13 +16,13 @@ function App() {
           <Sidenav />
         <main className="main">
           <div className="main-header">
-            <div className="main-header__heading">Hello User</div>
-            <div className="main-header__updates">Recent Items</div>
+            <div className="main-headerHeading">Hello User</div>
+            <div className="main-headerUpdates">Recent Items</div>
           </div>
         </main>
         <footer className="footer">
-          <div className="footer__copyright">&copy; 2020 Matthew Devonport</div>
-          <div className="footer__signature">Sample Text</div>
+          <div className="footerCopyright">&copy; 2020 Matthew Devonport</div>
+          <div className="footerSignature">Sample Text</div>
         </footer>
       </div>
     </React.Fragment>
