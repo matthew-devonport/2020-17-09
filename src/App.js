b74db8d27866824a973bbe9918e888369ecd349c
matthew-devonport <roarsintheeast@gmail.com>
2020-09-18 20:51:31 +1200

    started scaling sidenav to mobile

--- src/App.js
+++ src/App.js
@@ -6,30 +6,30 @@ function App() {
     <React.Fragment>
       <div className="grid-container">
         <header className="header">
-          <div class="header__search">Search...</div>
-          <div class="header__avatar">Icon</div>
+          <div className="header__search">Search...</div>
+          <div className="header__avatar">Icon</div>
         </header>
         <aside className="sidenav">
-          <ul class="sidenav__list">
-            <li class="sidenav__list-item">Item One</li>
-            <li class="sidenav__list-item">Item Two</li>
-            <li class="sidenav__list-item">Item Three</li>
-            <li class="sidenav__list-item">Item Four</li>
-            <li class="sidenav__list-item">Item Five</li>
+          <div className="mobileNavIcon">+</div>
+          <ul className="sidenav__list">
+            <li className="sidenav__list-item">Item One</li>
+            <li className="sidenav__list-item">Item Two</li>
+            <li className="sidenav__list-item">Item Three</li>
+            <li className="sidenav__list-item">Item Four</li>
+            <li className="sidenav__list-item">Item Five</li>
           </ul>
         </aside>
         <main className="main">
-          <div class="main-header">
-            <div class="main-header__heading">Hello User</div>
-            <div class="main-header__updates">Recent Items</div>
+          <div className="main-header">
+            <div className="main-header__heading">Hello User</div>
+            <div className="main-header__updates">Recent Items</div>
           </div>
         </main>
         <footer className="footer">
-          <div class="footer__copyright">&copy; 2020 Matthew Devonport</div>
-          <div class="footer__signature">PHEARNOC Digital</div>
+          <div className="footer__copyright">&copy; 2020 Matthew Devonport</div>
+          <div className="footer__signature">PHEARNOC Digital</div>
         </footer>
       </div>
-
     </React.Fragment>
   );
 }
