b334f4fe4b2ebc8af2f3e8481b2ec147cad1ab01
matthew-devonport <roarsintheeast@gmail.com>
2020-09-18 19:51:46 +1200

    added content to header/footer

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ function App() {
   return (
     <React.Fragment>
       <div className="grid-container">
-      <header className="header">
-      </header>
-      <aside className="sidenav"></aside>
-      <main className="main"></main>
-      <footer className="footer"></footer>
-    </div>
+        <header className="header">
+          <div class="header__search">Search...</div>
+          <div class="header__avatar">Icon</div>
+        </header>
+        <aside className="sidenav"></aside>
+        <main className="main"></main>
+        <footer className="footer">
+          <div class="footer__copyright">&copy; 2020 Matthew Devonport</div>
+          <div class="footer__signature">PHEARNOC Digital</div>
+        </footer>
+      </div>
 
     </React.Fragment>
   );
