2e20d2c9966d0f21a55834b9f277103718d50c84
matthew-devonport <roarsintheeast@gmail.com>
2020-09-20 12:25:50 +1200

    testing total prices works

--- src/tests/sales.test.js
+++ src/tests/sales.test.js
@@ -1,5 +1,7 @@
 const sales = require('../sales')
 
+// Declaring the sales array here as a way to show it on the test file, rather than importing.
+
 const array = [
     { itemSold: 'Football', price: 19.99, dateSold: '2018-04-07', id: 'j_123' },
     { itemSold: 'Trainers', price: 159.95, dateSold: '2018-03-02', id: 't_acds1' },
@@ -7,4 +9,10 @@ const array = [
     { itemSold: 'Rugby ball', price: 30.00, dateSold: '2017-04-22', id: 't_acds3' },
     { itemSold: 'Hockey stick', price: 54.95, dateSold: '2017-03-19', id: 'j_999' }
 ]
+
+
+// Testing to check if totalPrice function works.
+test('return total prices', () => {
+    expect(sales.totalPrice(array)).toBe("469.86")
+})
 
